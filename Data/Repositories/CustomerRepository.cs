cbd83a6bd4bd1f2211265a3938f129c196159e64
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-02-24 15:10:46 +0100

    Modified GetAllAsync in CustomerRepository to return a filtered customer list

--- Data/Repositories/CustomerRepository.cs
+++ Data/Repositories/CustomerRepository.cs
@@ -1,8 +1,20 @@
 ﻿using Data.Contexts;
 using Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data.Repositories;
 
 public class CustomerRepository(DataContext context) : BaseRepository<CustomerEntity>(context)
 {
+    public override async Task<IEnumerable<CustomerEntity>> GetAllAsync()
+    {
+        var entites = await _context.Customers
+            .Select(x => new CustomerEntity
+            {
+                Id = x.Id,
+                CustomerName = x.CustomerName,
+            }).ToListAsync();
+
+        return entites;
+    }
 }
